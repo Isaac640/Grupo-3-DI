e1aed8bb8eef07b2f68fe1b0525c5e933fcc6ced
Isaac640 <isaac.cabriadiez@iesmiguelherrero.com>
2023-02-17 20:14:46 +0100

    Cambios a login

--- Grupo_3_Intermodular/Login.cs
+++ Grupo_3_Intermodular/Login.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,52 +14,66 @@ namespace Grupo_3_Intermodular
 {
     public partial class FrmLogin : Form
     {
-        string usu = "isaac";
-        string con = "1234";
+        /// <summary>
+        /// Inicio de sesión del administrador
+        /// </summary>
+
         public FrmLogin()
         {
             InitializeComponent();
-            //Centrar la ventana a la pantalla al iniciar la aplicaciòn
-            this.StartPosition = FormStartPosition.CenterScreen;
         }
 
-        private void btnIniciarSesion_Click(object sender, EventArgs e)
+        private async void btnIniciarSesion_Click(object sender, EventArgs e)
         {
+
             //Si alguno del los campos estan vacios, se mostrara un MensajeBox mencionandolo
             if (string.IsNullOrEmpty(txtUsuario.Text) || string.IsNullOrEmpty(txtContrasenia.Text))
             {
                 MessageBox.Show("Uno de los campos esta vacío", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
 
-            if (verificarUsuario() == true)
+            //encriptar contraseña a MD5
+            EncriptadorMD5(txtContrasenia.Text);
+            //81dc9bdb52d04dc20036dbd8313ed055
+
+            /*Si funciona, verifica que los campos esten rellenados y que el usuario exista
+            *Usa la API, avanza al siguiente formulario y cierra la ventana de login.
+            *El metodo que hay en Program.cs hace que si hay mas ventanas por aparecer, cierra la ventana y sigue a la siguiente
+            *La siguiente ventana sera Mantenimientos de Guardias. SOLO ADMINISTRADORES PUEDEN ACCEDER
+            */
+
+            if (await Negocio.InicioSesion(txtUsuario.Text, txtContrasenia.Text) != 0)
             {
-                //Si va, tira la API y avanza al siguiente formulario, y cierra la ventana de login
-                //la siguiente ventana sera el listado de profesores SOLO ADMINS PUEDEN ACCEDER
-                
-                ContenedorMDI mdi = new ContenedorMDI();
-                mdi.Show();
+                MantenimientoGuardias mGuardias = new MantenimientoGuardias();
+                mGuardias.Show();
 
                 this.Close();
-            }
-
-        }
-
-        public bool verificarUsuario()
-        {
-            bool valido = false;
-
-            //Si el usuario y contraseña coinciden se pondra a true
-            //Si no, mostrará un texto di que es incorrecto
-            if (txtUsuario.Text.ToLower().Equals(usu) && txtContrasenia.Text.Equals(con))
-            {
-                valido = true;
             }
             else
             {
                 lblError.Text = "El Usuario o Contraseña son incorrectos";
             }
-            valido = true; //ACORDARSE DE QUITAR ESTO MUY IMPORTANTE
-            return valido;
+
+
+        }
+        public static string EncriptadorMD5(string contraseña)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+
+            //calcular hash a partir de los bytes de texto
+            md5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(contraseña));
+
+            //obtiene el resultado de hash despues de calcularlo
+            byte[] result = md5.Hash;
+
+            StringBuilder strBuilder = new StringBuilder();
+            for (int i = 0; i < result.Length; i++)
+            {
+                //cambiarlo a 2 digitos hexadecimales por cada byte
+                strBuilder.Append(result[i].ToString("x2"));
+            }
+
+            return strBuilder.ToString();
         }
     }
 }
